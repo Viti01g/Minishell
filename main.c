96e6b2a3c91ec2bdc1172878f4525307c253909d
Diego Rubio morato <diegorubiomorato@gmail.com>
2023-09-12 15:19:12 +0200

    fallo

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: drubio-m <drubio-m@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/12 15:16:19 by vruiz-go          #+#    #+#             */
-/*   Updated: 2023/09/12 15:16:50 by drubio-m         ###   ########.fr       */
+/*   Updated: 2023/09/12 15:19:03 by drubio-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,6 @@
 int	main(int argc, char **argv)
 {
 	if (argc != 1)
-		
+		return(EXIT_FAILURE);
 	return (EXIT_SUCCESS);
 }
