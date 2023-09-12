328316cdcf6ca28f1db165b418de8287a587173b
Diego Rubio morato <diegorubiomorato@gmail.com>
2023-09-12 15:17:23 +0200

    header

--- main.c
+++ main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vruiz-go <vruiz-go@student.42.fr>          +#+  +:+       +#+        */
+/*   By: drubio-m <drubio-m@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/12 15:16:19 by vruiz-go          #+#    #+#             */
-/*   Updated: 2023/09/12 15:16:20 by vruiz-go         ###   ########.fr       */
+/*   Updated: 2023/09/12 15:16:50 by drubio-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 
 int	main(int argc, char **argv)
 {
-	if ()
+	if (argc != 1)
+		
 	return (EXIT_SUCCESS);
 }
