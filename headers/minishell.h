4407a9b5580e17329bfd1bffbb6e7cf8d71f8a50
Victor Ruiz Gonzalez <vruiz-go@c1r8s5.42madrid.com>
2023-09-13 15:12:24 +0200

    Script parse

--- headers/minishell.h
+++ headers/minishell.h
@@ -1,19 +1,32 @@
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-# include <stdio.h>			/* Para printf */
+# include <stdio.h>			    /* Para printf */
 # include <stdlib.h>			/* Para malloc, free */
 # include <unistd.h>			/* Para write, access, open, read, close, fork, exit, getcwd, chdir */
-# include <sys/wait.h>		/* Para wait, waitpid, wait3, wait4 */
+# include <sys/wait.h>		    /* Para wait, waitpid, wait3, wait4 */
 # include <signal.h>			/* Para signal, sigaction, kill */
-# include <sys/stat.h>		/* Para stat, lstat, fstat */
-# include <fcntl.h>			/* Para unlink, execve, dup, dup2, pipe */
+# include <sys/stat.h>		    /* Para stat, lstat, fstat */
+# include <fcntl.h>		    	/* Para unlink, execve, dup, dup2, pipe */
 # include <dirent.h>			/* Para opendir, readdir, closedir */
 # include <string.h>			/* Para strerror */
-# include <errno.h>			/* Para perror */
-# include <termios.h>		/* Para tcsetattr, tcgetattr */
+# include <errno.h>			    /* Para perror */
+# include <termios.h>		    /* Para tcsetattr, tcgetattr */
 # include <curses.h>			/* Para tgetent, tgetflag, tgetnum, tgetstr, tgoto, tputs */
 # include "../libft/libft.h"
 
+# define CMD = 1;
+# define PIPE = 2;
+# define FLCH_IZQ = 3;
+# define DBL_FLCH_IZQ = 4;
+# define FLCH_DRCH = 5;
+# define DBL_FLCH_DRCH = 6;
+
+typedef struct s_token
+{
+    int     tipo;
+    char    *str;
+    t_token *new;
+}   t_token;
 
 #endif
---
+++ libft/parse.c
@@ -0,0 +1,6 @@
+# include "minishell.h"
+
+int separator(char **argv)
+{
+    
+}
