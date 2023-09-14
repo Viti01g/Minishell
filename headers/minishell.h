4f04f573daad750eb8abb7c8d6e812549dcdb4ef
Victor Ruiz Gonzalez <vruiz-go@c2r1s6.42madrid.com>
2023-09-14 13:09:40 +0200

    Readline

--- headers/minishell.h
+++ headers/minishell.h
@@ -1,7 +1,9 @@
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-# include <stdio.h>			    /* Para printf */
+# include <stdio.h>             /* Para printf */
+# include <readline/readline.h> 
+# include <readline/history.h>
 # include <stdlib.h>			/* Para malloc, free */
 # include <unistd.h>			/* Para write, access, open, read, close, fork, exit, getcwd, chdir */
 # include <sys/wait.h>		    /* Para wait, waitpid, wait3, wait4 */
@@ -15,6 +17,14 @@
 # include <curses.h>			/* Para tgetent, tgetflag, tgetnum, tgetstr, tgoto, tputs */
 # include "../libft/libft.h"
 
+# define GREEN "\e[1;32m"
+# define RESET "\e[0m"
+# define RED "\e[1;91m"
+# define CYAN "\e[1;36m"
+# define YELLOW "\e[1;33m"
+# define PURPLE "\e[1;35m"
+# define BLUE "\e[1;34m"
+
 # define CMD = 1;
 # define PIPE = 2;
 # define FLCH_IZQ = 3;
@@ -26,7 +36,7 @@ typedef struct s_token
 {
     int     tipo;
     char    *str;
-    t_token *new;
+    struct s_token *new;
 }   t_token;
 
 #endif
--- main.c
+++ main.c
@@ -1,8 +1,23 @@
 # include "headers/minishell.h"
 
-int	main(int argc, char **argv, char **env)
+void	view_prompt(void)
 {
+	write(1, GREEN, ft_strlen(GREEN));
+	write(1, "minishell$ ", strlen("minishell$ "));
+	write(1, RESET, ft_strlen(RESET));
+}
+
+int	main(int argc, char **argv)
+{
+	char *view;
+
+	view = "a";
 	if (argc != 1)
 		exit(EXIT_FAILURE);
+	while (1)
+	{
+		// view_prompt();
+		view = readline("\e[1;32mminishell$ \e[0m");
+	}
 	return (EXIT_SUCCESS);
 }
