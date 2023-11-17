a8fdb393f2794fc93a2f8a3aee201a1b333b1820
Diego Mac_42 <diegorubiomorato@gmail.com>
2023-11-17 15:22:34 +0100

    cambios

--- src/diego.c
+++ src/diego.c
@@ -7,7 +7,7 @@ void ft_leaks(void)
 	system("leaks -q minishell");
 }
 
-void split_token(char *input)
+/* void split_token(char *input)
 {
 	t_token *tokens;
 	int	i;
@@ -23,20 +23,20 @@ void split_token(char *input)
 		{
 			fragment = ft_substr(input, j, i - j);
 			ft_lstnew_addback(&tokens, fragment);
-			printf("%s\n", tokens->str);
+		//	printf("%s\n", tokens->str);
 			free(fragment);
 			if ((input[i] == '>' || input[i] == '<') && (input[i] == input[i + 1]))
 			{
 				fragment = ft_substr(input, i, 2);
 				ft_lstnew_addback(&tokens, fragment);
-				printf("%s\n", tokens->next->str);
+		//		printf("%s\n", tokens->next->str);
 				i++;
 			}
 			else
 			{
             	fragment = ft_substr(input, i, 1);
 				ft_lstnew_addback(&tokens, fragment);
-				printf("%s\n", tokens->next->str);
+		//		printf("%s\n", tokens->next->str);
 			}
 			free(fragment);
 			j = i + 1;
@@ -45,11 +45,59 @@ void split_token(char *input)
     }
     fragment = ft_substr(input, j, i - j);
 	ft_lstnew_addback(&tokens, fragment);
-	printf("%s\n", tokens->next->next->str);
+//	printf("%s\n", tokens->next->next->str);
 	free(fragment);
+} */
+
+int is_simple_operator(char c)
+{
+    return (c == '|' || c == '<' || c == '>');
 }
 
-int	main(void) 
+// Función para identificar operadores dobles
+int is_double_operator(char c, char next)
+{
+    return ((c == '>' || c == '<') && (c == next));
+}
+
+// Función para obtener fragmentos y crear tokens
+void process_tokens(t_token **tokens, char *input, int start, int end)
+{
+    char *fragment = ft_substr(input, start, end - start);
+    ft_lstnew_addback(tokens, fragment);
+    free(fragment);
+}
+
+// Función principal para dividir tokens
+void split_token(char *input)
+{
+    t_token *tokens = NULL;
+    int i = 0;
+    int j = 0;
+
+    while (input[i]) {
+        if (is_simple_operator(input[i])) {
+            process_tokens(&tokens, input, j, i);
+            free(ft_substr(input, i, 1)); // Tratar operador simple como un token
+            j = i + 1;
+        } else if (is_double_operator(input[i], input[i + 1])) {
+            process_tokens(&tokens, input, j, i);
+            free(ft_substr(input, i, 2)); // Tratar operador doble como un token
+            i++;
+            j = i + 1;
+        }
+        i++;
+    }
+    process_tokens(&tokens, input, j, i); // Agregar el último token
+    // Aquí podrías iterar sobre 'tokens' para hacer alguna operación con los tokens
+	t_token *current = tokens;
+	while (current) {
+    	printf("%s (%p)\n", current->str, current);
+    	current = current->next;
+	}
+}
+
+int	main(void)
 {
     char *input;
 
