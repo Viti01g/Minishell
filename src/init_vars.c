3c6019b050dd77bf28ab43680c76a7c982bb2ee6
Viti <vruizgonz@gmail.com>
2023-11-27 16:03:20 +0100

    Cosas de builtins

---
+++ src/init_vars.c
@@ -0,0 +1,12 @@
+#include "../headers/minishell.h"
+
+void	init_vars(t_general *gen, char **env)
+{
+	char	path[PATH_MAX];
+
+	gen->env = ft_cpy_env(env);
+	gen->env_path = ft_cpy_path(env);
+	gen->env_home = ft_cpy_home(env);
+	gen->env_pwd = ft_strdup(getcwd(path, sizeof(path)));
+	gen->env_oldpwd = ft_cpy_oldpwd(env);
+}
---
+++ src/probar_cosas.c
@@ -0,0 +1,29 @@
+# include "../headers/minishell.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main() {
+    // Tamaño del buffer para almacenar el directorio actual
+    size_t buffer_size = 1024;
+
+    // Buffer para almacenar el directorio actual
+    char *current_directory = (char *)malloc(buffer_size);
+    if (current_directory == NULL) {
+        perror("Error al asignar memoria para el directorio actual");
+        exit(EXIT_FAILURE);
+    }
+
+    // Obtener el directorio actual
+    if (getcwd(current_directory, buffer_size) != NULL) {
+        printf("Directorio actual: %s\n", current_directory);
+    } else {
+        perror("Error al obtener el directorio actual");
+        exit(EXIT_FAILURE);
+    }
+
+    // Liberar la memoria asignada al buffer
+    free(current_directory);
+
+    return EXIT_SUCCESS;
+}
