d685b118e133ca9e125b35292fc6f0811e64c5f1
Diego Mac_42 <diegorubiomorato@gmail.com>
2023-12-04 12:26:44 +0100

    new main

--- src/diego.c
+++ src/diego.c
@@ -2,46 +2,31 @@
 
 void ft_leaks(void)
 {
-	system("leaks -q minishell");
+    system("leaks -q minishell");
 }
 
-/* void free_tokens(t_token *tokens)
-{
-    t_token *tmp;
-
-    while (tokens) {
-        tmp = tokens;
-        tokens = tokens->next;
-        tmp->str = NULL;
-        free(tmp);
-    }
-} */
 void free_tokens(t_token *tokens)
 {
     t_token *tmp;
+    int     i;
 
     while (tokens)
     {
         tmp = tokens;
         tokens = tokens->next;
-
-        // Liberar la doble matriz
-        for (int i = 0; tmp->str[i] != NULL; i++)
-        {
+        i = -1;
+        while(tmp->str[++i])
             free(tmp->str[i]);
-        }
         free(tmp->str);
-
         free(tmp);
     }
 }
-
 
 void process_simple_operator(char *input, t_token **tokens, int *i, int *j)
 {
     char *fragment;
 
-	fragment = ft_substr(input, *j, *i - *j);
+    fragment = ft_substr(input, *j, *i - *j);
     ft_lstnew_addback(tokens, fragment);
    // printf("%s\n", (*tokens)->str);
     free(fragment);
@@ -51,22 +36,22 @@ void process_double_operator(char *input, t_token **tokens, int *i)
 {
     char *fragment;
 
-	fragment = ft_substr(input, *i, 2);
+    fragment = ft_substr(input, *i, 2);
     ft_lstnew_addback(tokens, fragment);
-//	if ((*tokens)->next != NULL)
-	  	//printf("%s\n", (*tokens)->next->str);
+//  if ((*tokens)->next != NULL)
+        //printf("%s\n", (*tokens)->next->str);
     (*i)++;
-	free(fragment);
+    free(fragment);
 }
 
 void process_single_operator(char *input, t_token **tokens, int *i)
 {
     char *fragment;
 
-	fragment = ft_substr(input, *i, 1);
+    fragment = ft_substr(input, *i, 1);
     ft_lstnew_addback(tokens, fragment);
-	//if ((*tokens)->next != NULL)
-    //	printf("%s\n", (*tokens)->next->str);
+    //if ((*tokens)->next != NULL)
+    //  printf("%s\n", (*tokens)->next->str);
     free(fragment);
 }
 
@@ -75,26 +60,26 @@ void split_token(char *input, t_token **tokens)
     int i;
     int j;
 
-	i = 0;
-	j = 0;
+    i = 0;
+    j = 0;
     while (input[i])
-	{
+    {
         if (input[i] == '|' || input[i] == '<' || input[i] == '>') 
-		{
-			process_simple_operator(input, tokens, &i, &j);
+        {
+            process_simple_operator(input, tokens, &i, &j);
             if ((input[i] == '>' || input[i] == '<') && (input[i] == input[i + 1]))
                 process_double_operator(input, tokens, &i);
-			else 
+            else 
                 process_single_operator(input, tokens, &i);
             j = i + 1;
         }
         i++;
     }
-	if (j < i)
-		process_simple_operator(input, tokens, &i, &j);
+    if (j < i)
+        process_simple_operator(input, tokens, &i, &j);
 }
 
-int main(void)
+/* int main(void)
 {
     char *input;
     t_token *tokens;
@@ -123,4 +108,41 @@ int main(void)
         tokens = NULL;
     }
     return 0;
+} */
+
+int main(void)
+{
+    char *input;
+    t_token *tokens;
+
+    tokens = NULL;
+    atexit(ft_leaks);
+
+    while (1) 
+    {
+        input = readline("\e[1;32mminishell$ \e[0m");
+        printf("%p\n", input);
+        if (!input) 
+            break ;
+        split_token(input, &tokens);
+        free(input);
+        t_token *current_token = tokens;
+        int token_index = 1;
+        while (current_token != NULL)
+        {
+            printf("\nToken %d\n", token_index);
+            char **current_words = current_token->str;
+            int word_index = 1;
+            for (int i = 0; current_words[i] != NULL; i++)
+            {
+                printf("Word %d: %s\n", word_index++, current_words[i]);
+            }
+            current_token = current_token->next;
+            token_index++;
+        }
+        free_tokens(tokens);
+        tokens = NULL;
+    }
+    return 0;
 }
+
