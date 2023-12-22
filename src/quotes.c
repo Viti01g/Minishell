8c7df25230f32f151e023e27ffc837efe18a10b0
Diego Mac_42 <diegorubiomorato@gmail.com>
2023-12-22 20:40:50 +0100

    some changes to double quoting

--- src/quotes.c
+++ src/quotes.c
@@ -3,6 +3,59 @@
 // TODO: Manejar que me detecte los argumentos de los comandos bien
 	// TODO: ejemplo: echo "hola mundo" solo tiene un arg no 2
 
+// TODO: Manejar redirecciones y pipes dentro de las comillas
+// TODO: Hacer una función que se llame double quote su funcionamiento es así:
+		// * Al tokenizar si encuentra una comilla doble, va a fumarse todo hasta
+		// * hasta la siguiente comilla doble y va a llamar en paralelo a double quote
+		// * la cual devuelve un char * que sería el nuevo token ole ole ole
+
+
+void ft_leaks()
+{
+	system("leaks -q minishell");
+}
+
+/* void	select_quote_mode(char *input)
+{
+	int	i;
+
+	i = 0;
+	while (input[i])
+	{
+
+		i++;
+	}
+} */
+
+char	*retrieve_double_quote(char *input)
+{
+	int		i;
+	int		start;
+	int		stop;
+	char	*token;
+
+	i = -1;
+	start = 0;
+	stop = 0;
+	while (input[++i])
+	{
+		if (input[i] == '\"')
+		{
+			start = i;
+			i++;
+			while (input[i] != '\"')
+				i++;
+			stop = i;
+			printf("Esto vale start: %d\n", start);
+			printf("Esto vale stop: %d\n", stop);
+			break ;
+		}
+	}
+	token = ft_substr(input, start + 1, (stop - start) - 1);
+	printf("Este es tu token mi rey: %s\n", token);
+	return (token);
+}
+
 int	change_quote_state(int quote)
 {
 	if (quote == FALSE)
@@ -51,29 +104,24 @@ void	check_quotes(char *input)
 		until_double(input, &i, &double_flag);
 		until_single(input, &i, &single_flag);
 	}
-	if (double_flag == FALSE)
-		printf("Comillas dobles cerradas o no hay\n");
-	if (double_flag == TRUE)
-		printf("Comillas dobles abiertas\n");
-	if (single_flag == FALSE)
-		printf("Comillas simples cerradas o no hay\n");
-	if (single_flag == TRUE)
-		printf("Comillas simples abiertas\n");
 }
 
 int	main(void)
 {
 	char	*input;
-
+	char	*si;
 	while (1)
 	{
 		input = readline("\e[1;32mminishell$ \e[0m");
 		printf("%p\n", input);
 		if (!input || !ft_strcmp(input, "exit"))
 			break ;
-		check_quotes(input);
+		//check_quotes(input);
+		si = retrieve_double_quote(input);
+		free(si);
 		free(input);
 	}
-	system("leaks -q a.out");
+	//system("leaks -q minishell");
+	atexit(ft_leaks);
 	return (0);
 }
