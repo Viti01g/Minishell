81a6a85d689d335395d527516e8f8de6a127a08e
Diego Rubio <diegorubiomorato@gmail.com>
2024-01-07 20:22:26 +0100

    changes in word_count with quotes

--- libft/ft_split_cmd.c
+++ libft/ft_split_cmd.c
@@ -6,7 +6,7 @@
 /*   By: drubio-m <drubio-m@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/02 20:11:09 by drubio-m          #+#    #+#             */
-/*   Updated: 2024/01/07 20:21:02 by drubio-m         ###   ########.fr       */
+/*   Updated: 2024/01/07 20:22:00 by drubio-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,74 +26,6 @@ int	skip_spaces(char *str)
 		j++;
 	return (j);
 }
-
-/* static size_t	process_quotes(char *str, size_t i, char del)
-{
-    size_t	j;
-
-    j = i;
-    i++;
-    while (str[i] != del && str[i])
-        i++;
-    if (str[i] == del)
-        i++;
-    return (i - j);
-}
-
-static size_t	ft_count_words(char const *s, char c)
-{
-    size_t	word_count;
-    size_t	i;
-    int		in_quotes;
-    char	current_quote;
-
-    word_count = 0;
-    i = 0;
-    in_quotes = 0;
-    current_quote = '\0';
-	i += skip_spaces((char *)s);
-    while (s[i])
-    {
-        if (s[i] == '\'' || s[i] == '\"') 
-		{
-            if (in_quotes && current_quote == s[i])
-                in_quotes = 0;
-			else if (!in_quotes) 
-			{
-                in_quotes = 1;
-                current_quote = s[i];
-            }
-        }
-        if (!in_quotes && s[i] == c) 
-		{
-            word_count++;
-            while (s[i] && s[i] == c)
-                i++;
-        } 
-		else 
-            i++;
-    }
-    if (s[i - 1] != c)
-        word_count++;
-    printf("Esta es la cuenta: %zu\n", word_count);
-    return (word_count);
-} */
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 static void	process_quotes(char const *s, size_t *i, int *in_quotes, char *current_quote)
 {
@@ -145,41 +77,6 @@ static size_t	ft_count_words(char const *s, char c)
 	printf("Esta es la cuenta: %zu\n", word_count);
     return (word_count);
 }
-
-
-/* static size_t	ft_count_words(char const *s, char c)
-{
-    size_t	word_count;
-    size_t	i;
-    int		in_quotes;
-    char	current_quote;
-
-    word_count = 0;
-    i = 0;
-    in_quotes = 0;
-    current_quote = '\0';
-    i += skip_spaces((char *)s);
-    while (s[i])
-    {
-        if (s[i] == '\'' || s[i] == '\"') 
-		{
-            process_quotes(s, &i, &in_quotes, &current_quote);
-            continue ;
-        }
-        if (!in_quotes && s[i] == c) 
-		{
-            word_count++;
-            while (s[i] && s[i] == c)
-                i++;
-        } 
-		else 
-            i++;
-    }
-    if (s[i - 1] != c)
-        word_count++;
-    printf("Esta es la cuenta: %zu\n", word_count);
-    return (word_count);
-} */
 
 static void	ft_free_split(char **str, size_t words)
 {
--- src/quotes.c
+++ src/quotes.c
@@ -15,6 +15,21 @@
 	system("leaks -q minishell");
 } */
 
+int	is_whitespace(char c)
+{
+	return (c == ' ' || (c > 8 && c < 14));
+}
+
+int	skip_spaces(char *str, int i)
+{
+	int	j;
+
+	j = 0;
+	while (is_whitespace(str[i + j]))
+		j++;
+	return (j);
+}
+
 int	process_quotes(char *str, int i, char del)
 {
 	int	j;
