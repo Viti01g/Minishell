1cc9299e888701f6455970585980b63bc516824b
Diego Mac_42 <diegorubiomorato@gmail.com>
2024-04-25 19:06:25 +0200

    norminette passed in categorize_arguments

--- src/parser/categorize_arguments.c
+++ src/parser/categorize_arguments.c
@@ -1,45 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   categorize_arguments.c                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: drubio-m <drubio-m@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/25 19:02:06 by drubio-m          #+#    #+#             */
+/*   Updated: 2024/04/25 19:05:28 by drubio-m         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minishell.h"
 
-void handle_syntax_error(t_token *current_node)
+void	handle_syntax_error(t_token *current_node)
 {
-    if (current_node == NULL)
-    {
-        printf("Syntax error\n");
-        exit(1);
-    }
+	if (current_node == NULL)
+	{
+		printf("Syntax error\n");
+		exit(1);
+	}
 }
 
-void set_node_type(t_token *current_node)
+void	set_node_type(t_token *current_node)
 {
-    if (is_command(current_node->str[0]) || access(current_node->str[0], X_OK) == 0)
-        current_node->type = CMD;
-    else
-        current_node->type = OUTFILE;
+	if (is_command(current_node->str[0])
+		|| access(current_node->str[0], X_OK) == 0)
+		current_node->type = CMD;
+	else
+		current_node->type = OUTFILE;
 }
 
-void handle_redirection(t_token **current_node)
+void	handle_redirection(t_token **current_node)
 {
-    *current_node = (*current_node)->next;
-    handle_syntax_error(*current_node);
-    set_node_type(*current_node);
+	*current_node = (*current_node)->next;
+	handle_syntax_error(*current_node);
+	set_node_type(*current_node);
 }
 
-void categorize_arguments(t_token *tokens)
+void	categorize_arguments(t_token *tokens)
 {
-    t_token *current_node;
+	t_token	*current_node;
 
-    current_node = tokens;
-    while (current_node != NULL)
-    {
-        if (strcmp(current_node->str[0], ">") == 0 || strcmp(current_node->str[0], ">>") == 0)
-        {
-            handle_redirection(&current_node);
-        }
-        else if (strcmp(current_node->str[0], "<") == 0)
-        {
-            handle_redirection(&current_node);
-            current_node->type = INFILE;
-        }
-        current_node = current_node->next;
-    }
+	current_node = tokens;
+	while (current_node != NULL)
+	{
+		if (strcmp(current_node->str[0], ">") == 0
+			|| strcmp(current_node->str[0], ">>") == 0)
+		{
+			handle_redirection(&current_node);
+		}
+		else if (strcmp(current_node->str[0], "<") == 0)
+		{
+			handle_redirection(&current_node);
+			current_node->type = INFILE;
+		}
+		current_node = current_node->next;
+	}
 }
